553bab2852625999d9ad90e220addf572921e4fc
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-03-09 11:54:42 +0700

    Update authentication.component.ts

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder,FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { MatDialogRef,MatDialog } from '@angular/material/dialog';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-authentication',
@@ -15,27 +15,27 @@ export class AuthenticationComponent {
   constructor(
     public dialogRef: MatDialogRef<AuthenticationComponent>,
     public dialog: MatDialog,
-    private formBuilder : FormBuilder, private http: HttpClient, private router : Router){}
+    private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
 
-  public ngOnInit(): void {  
-    this.loginForm  = this.formBuilder.group({
-      username : [''],
-      password : ['']
-    }) 
+  public ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    })
   }
 
   public onCloseClick(): void {
     this.dialogRef.close();
   }
-  
-  public onPasswordSubmit(): void{
+
+  public onPasswordSubmit(): void {
     (this.loginForm.value.password)
-      if (this.loginForm.value.password == "admin") {
-        alert("successful!");
-        this.loginForm.reset();
-        this.dialog.closeAll();
-      } else {
-        alert("Invalid password.");
-      }
-    };
-  }
+    if (this.loginForm.value.password == "admin") {
+      alert("successful!");
+      this.loginForm.reset();
+      this.dialog.closeAll();
+    } else {
+      alert("Invalid password.");
+    }
+  };
+}
