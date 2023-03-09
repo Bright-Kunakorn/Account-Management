f53712f03912952a0d9b6a4f1adf5875c54611a5
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-03-10 01:01:31 +0700

    Update edit-popup.component.ts

--- src/app/edit-popup/edit-popup.component.ts
+++ src/app/edit-popup/edit-popup.component.ts
@@ -12,26 +12,25 @@ interface Role {
   templateUrl: './edit-popup.component.html',
   styleUrls: ['./edit-popup.component.css']
 })
-
 export class EditPopupComponent {
-  constructor(public dialog: MatDialog) {
-  }
   roles: Role[] = [
     { value: 'accountant', viewValue: 'Accountant' },
     { value: 'market-manager', viewValue: 'Market Manager' },
     { value: 'software-engineer', viewValue: 'Software Engineer' },
     { value: 'marketing-coordinator', viewValue: 'Marketing Coordinator' },
-    { value: 'software-developer	', viewValue: 'Software Developer' },
+    { value: 'software-developer', viewValue: 'Software Developer' },
     { value: 'sales-manager', viewValue: 'Sales Manager' },
     { value: 'human-resources-manager', viewValue: 'Human Resources Manager' },
     { value: 'sales-representative', viewValue: 'Sales Representative' },
-
   ];
-  public onCloseClick(): void {
+
+  constructor(private dialog: MatDialog) {}
+
+  onCloseClick(): void {
     this.dialog.closeAll();
   }
-  public openDialogAuthent(): void {
-    this.dialog.open(AuthenticationComponent)
-  }
 
+  openDialogAuthent(): void {
+    this.dialog.open(AuthenticationComponent);
+  }
 }
