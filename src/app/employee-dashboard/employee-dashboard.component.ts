86aa62db9e2dfc0dc602074460e5005332dbcdd5
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-03-09 11:53:14 +0700

    Update employee-dashboard.component.ts

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -11,21 +11,21 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/internal/Subscription';
 
 interface Employee {
-  id: number; 
-  first_name: string; 
+  id: number;
+  first_name: string;
   last_name: string;
-  email: string; 
-  phone: string; 
-  avatar: string; 
-  street: string; 
-  city: string; 
+  email: string;
+  phone: string;
+  avatar: string;
+  street: string;
+  city: string;
   department: string;
   job_title: string;
-  gender: string; 
-  salary: string; 
-  hireDate: string; 
-  birthDate: string; 
-  educate: string; 
+  gender: string;
+  salary: string;
+  hireDate: string;
+  birthDate: string;
+  educate: string;
 }
 export interface DialogData {
   selected: number;
@@ -47,12 +47,12 @@ const EMPLOYEE_DATA: Employee[] = employeeData;
 })
 
 export class EmployeeDashboardComponent {
-  displayedColumns = ['id', 'first_name', 'email', 'job_title', 'department', 'salary', 'hireDate' ,'icon'];
+  displayedColumns = ['id', 'first_name', 'email', 'job_title', 'department', 'salary', 'hireDate', 'icon'];
   dataSource = new MatTableDataSource(EMPLOYEE_DATA);
   employees: Employee[] = employeeData;
   collectionSize = this.employees.length;
   employee: Employee[];
-  public   id: number;
+  public id: number;
   selected: number;
   routeQueryParams$: Subscription;
 
@@ -67,24 +67,24 @@ export class EmployeeDashboardComponent {
       }
     });
   }
-  
+
   private employeeInfo: EmployeeInfoComponent;
-  
+
   public ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
   public applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); 
-    filterValue = filterValue.toLowerCase(); 
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
   }
   public openDialogDel(ID: number): void {
     const dialogRef = this.dialogRef.open(DeletePopupComponent, {
-      data: {selected: ID}
+      data: { selected: ID }
     });
-  
+
     dialogRef.afterOpened().subscribe(result => {
       this.selected = ID;
       this.router.navigate(['.'], { relativeTo: this.route });
@@ -93,7 +93,7 @@ export class EmployeeDashboardComponent {
   public getID() {
     return this.id;
   }
-  public setID(ID: number){
+  public setID(ID: number) {
     this.id = ID;
   }
 
@@ -102,21 +102,21 @@ export class EmployeeDashboardComponent {
   }
   public openDialogInfo(ID: number): void {
     const dialogRef = this.dialogRef.open(EmployeeInfoComponent, {
-      data: {selected: ID}
+      data: { selected: ID }
     });
-  
+
     dialogRef.afterOpened().subscribe(result => {
       this.selected = ID;
       this.router.navigate(['.'], { relativeTo: this.route });
     });
   }
-  
+
   public goToTop() {
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
-}
+  }
 }
 
 
