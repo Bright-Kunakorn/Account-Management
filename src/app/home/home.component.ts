c86110680eadb0d7dceb3303ecd32a92f57f6c16
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-03-10 00:23:35 +0700

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,56 +22,116 @@ interface Employee {
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-
 export class HomeComponent {
   count: number;
 
-  filteredBusinessDev = employeeData.filter((obj) => obj.department === "Business Development");
-  filteredProductManage = employeeData.filter((obj) => obj.department === "Product Management");
-  filteredSupport = employeeData.filter((obj) => obj.department === "Support");
-  filteredAccount = employeeData.filter((obj) => obj.department === "Accounting");
-  filteredTraining = employeeData.filter((obj) => obj.department === "Training");
-  filteredLegal = employeeData.filter((obj) => obj.department === "Legal");
-  filteredSales = employeeData.filter((obj) => obj.department === "Sales");
-  filteredServices = employeeData.filter((obj) => obj.department === "Services");
-  filteredResearchDev = employeeData.filter((obj) => obj.department === "Research and Development");
-  fillterData = [this.filteredBusinessDev.length, this.filteredProductManage.length, this.filteredSupport.length,
-  this.filteredAccount.length, this.filteredTraining.length, this.filteredLegal.length, this.filteredSales.length
-    , this.filteredServices.length, this.filteredResearchDev.length]
+  filteredBusinessDev = employeeData.filter(
+    (obj) => obj.department === 'Business Development'
+  );
+  filteredProductManage = employeeData.filter(
+    (obj) => obj.department === 'Product Management'
+  );
+  filteredSupport = employeeData.filter((obj) => obj.department === 'Support');
+  filteredAccount = employeeData.filter(
+    (obj) => obj.department === 'Accounting'
+  );
+  filteredTraining = employeeData.filter(
+    (obj) => obj.department === 'Training'
+  );
+  filteredLegal = employeeData.filter((obj) => obj.department === 'Legal');
+  filteredSales = employeeData.filter((obj) => obj.department === 'Sales');
+  filteredServices = employeeData.filter(
+    (obj) => obj.department === 'Services'
+  );
+  filteredResearchDev = employeeData.filter(
+    (obj) => obj.department === 'Research and Development'
+  );
+  fillterData = [
+    this.filteredBusinessDev.length,
+    this.filteredProductManage.length,
+    this.filteredSupport.length,
+    this.filteredAccount.length,
+    this.filteredTraining.length,
+    this.filteredLegal.length,
+    this.filteredSales.length,
+    this.filteredServices.length,
+    this.filteredResearchDev.length,
+  ];
 
   maxNumber = Math.max(...this.fillterData);
 
-
   private data = [
-    { "Framework": "Business Development", "Stars": this.filteredBusinessDev.length, "Released": "1" },
-    { "Framework": "Product Management", "Stars": this.filteredProductManage.length, "Released": "2" },
-    { "Framework": "Support", "Stars": this.filteredSupport.length, "Released": "3" },
-    { "Framework": "Accounting", "Stars": this.filteredAccount.length, "Released": "4" },
-    { "Framework": "Training", "Stars": this.filteredTraining.length, "Released": "5" },
-    { "Framework": "Legal", "Stars": this.filteredLegal.length, "Released": "6" },
-    { "Framework": "Sales", "Stars": this.filteredSales.length, "Released": "7" },
-    { "Framework": "Services", "Stars": this.filteredServices.length, "Released": "8" },
-    { "Framework": "Research and Development", "Stars": this.filteredResearchDev.length, "Released": "9" },
-
+    {
+      Framework: 'Business Development',
+      Stars: this.filteredBusinessDev.length,
+      Released: '1',
+    },
+    {
+      Framework: 'Product Management',
+      Stars: this.filteredProductManage.length,
+      Released: '2',
+    },
+    { Framework: 'Support', Stars: this.filteredSupport.length, Released: '3' },
+    {
+      Framework: 'Accounting',
+      Stars: this.filteredAccount.length,
+      Released: '4',
+    },
+    {
+      Framework: 'Training',
+      Stars: this.filteredTraining.length,
+      Released: '5',
+    },
+    { Framework: 'Legal', Stars: this.filteredLegal.length, Released: '6' },
+    { Framework: 'Sales', Stars: this.filteredSales.length, Released: '7' },
+    {
+      Framework: 'Services',
+      Stars: this.filteredServices.length,
+      Released: '8',
+    },
+    {
+      Framework: 'Research and Development',
+      Stars: this.filteredResearchDev.length,
+      Released: '9',
+    },
   ];
   employees: Employee[] = employeeData;
 
   public barChartOptions: any = {
     scaleShowVerticalLines: false,
-    responsive: true
+    responsive: true,
   };
 
-  public mbarChartLabels: string[] = ['Business Development', 'Product Management', 'Support', 'Accounting', 'Training', 'Legal', 'Services', 'Research and Development'];
+  public mbarChartLabels: string[] = [
+    'Business Development',
+    'Product Management',
+    'Support',
+    'Accounting',
+    'Training',
+    'Legal',
+    'Services',
+    'Research and Development',
+  ];
   public barChartType: string = 'bar';
   public barChartLegend: boolean = true;
 
   public barChartData: any[] = [
     {
-      data: [this.filteredBusinessDev.length, this.filteredProductManage.length, this.filteredSupport.length,
-      this.filteredAccount.length, this.filteredTraining.length, this.filteredLegal.length, this.filteredSales.length
-        , this.filteredServices.length, this.filteredResearchDev.length], label: 'Employee Member', backgroundColor: '#6783cc'
+      data: [
+        this.filteredBusinessDev.length,
+        this.filteredProductManage.length,
+        this.filteredSupport.length,
+        this.filteredAccount.length,
+        this.filteredTraining.length,
+        this.filteredLegal.length,
+        this.filteredSales.length,
+        this.filteredServices.length,
+        this.filteredResearchDev.length,
+      ],
+      label: 'Employee Member',
+      backgroundColor: '#6783cc',
     },
   ];
 
