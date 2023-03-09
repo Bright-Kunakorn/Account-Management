bdc08b3b5e9bfed2feb57de4ace72098e8627b21
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-03-09 10:22:12 +0700

    update Visualization Data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import employeeData from '../employee.json';
 
+interface Employee {
+  id: number; 
+  first_name: string; 
+  last_name: string;
+  email: string; 
+  phone: string; 
+  avatar: string; 
+  street: string; 
+  city: string; 
+  department: string;
+  job_title: string;
+  gender: string; 
+  salary: string; 
+  hireDate: string; 
+  birthDate: string; 
+  educate: string; 
+}
 
 @Component({
   selector: 'app-home',
@@ -9,26 +27,45 @@ import * as d3 from 'd3';
 })
 
 export class HomeComponent implements OnInit {
+  count:number;
   ngOnInit(): void {
     this.createSvg();
     this.drawPlot();
   }
+  filteredBusinessDev = employeeData.filter((obj) => obj.department === "Business Development");
+  filteredProductManage = employeeData.filter((obj) => obj.department === "Product Management");
+  filteredSupport = employeeData.filter((obj) => obj.department === "Support");
+  filteredAccount = employeeData.filter((obj) => obj.department === "Accounting");
+  filteredTraining = employeeData.filter((obj) => obj.department === "Training");
+  filteredLegal = employeeData.filter((obj) => obj.department === "Legal");
+  filteredSales = employeeData.filter((obj) => obj.department === "Sales");
+  filteredServices = employeeData.filter((obj) => obj.department === "Services");
+  filteredResearchDev = employeeData.filter((obj) => obj.department === "Research and Development");
+  fillterData = [this.filteredBusinessDev.length, this.filteredProductManage.length, this.filteredSupport.length, 
+                this.filteredAccount.length, this.filteredTraining.length, this.filteredLegal.length, this.filteredSales.length
+              , this.filteredServices.length, this.filteredResearchDev.length ]
+
+  maxNumber = Math.max(...this.fillterData);
+
+
   private data = [
-    {"Framework": "Business Development", "Stars": "45", "Released": "1"},
-    {"Framework": "Product Management", "Stars": "80", "Released": "2"},
-    {"Framework": "Support", "Stars": "4", "Released": "3"},
-    {"Framework": "Accounting", "Stars": "25", "Released": "4"},
-    {"Framework": "Training", "Stars": "13", "Released": "5"},
-    {"Framework": "Legal", "Stars": "52", "Released": "6"},
-    {"Framework": "Sales", "Stars": "69", "Released": "7"},
-    {"Framework": "Services", "Stars": "10", "Released": "8"},
-    {"Framework": "Research and Development", "Stars": "95", "Released": "9"},
+    {"Framework": "Business Development", "Stars": this.filteredBusinessDev.length, "Released": "1"},
+    {"Framework": "Product Management", "Stars": this.filteredProductManage.length, "Released": "2"},
+    {"Framework": "Support", "Stars": this.filteredSupport.length, "Released": "3"},
+    {"Framework": "Accounting", "Stars": this.filteredAccount.length, "Released": "4"},
+    {"Framework": "Training", "Stars": this.filteredTraining.length, "Released": "5"},
+    {"Framework": "Legal", "Stars": this.filteredLegal.length, "Released": "6"},
+    {"Framework": "Sales", "Stars": this.filteredSales.length, "Released": "7"},
+    {"Framework": "Services", "Stars": this.filteredServices.length, "Released": "8"},
+    {"Framework": "Research and Development", "Stars": this.filteredResearchDev.length, "Released": "9"},
 
   ];
+  employees: Employee[] = employeeData;
   private svg: d3.Selection<SVGGElement, unknown, HTMLElement, any>;
   private margin = 50;
-  private width = 750 - (this.margin * 2);
-  private height = 400 - (this.margin * 2);
+  private width = 900 - (this.margin * 2);
+  private height = 500 - (this.margin * 2);
+
 
   private createSvg(): void {
     this.svg = d3.select("figure#scatter")
@@ -50,7 +87,7 @@ export class HomeComponent implements OnInit {
 
     // Add Y axis
     const y = d3.scaleLinear()
-    .domain([0, 100])
+    .domain([0, this.maxNumber + 5 ])
     .range([ this.height, 0]);
     this.svg.append("g")
     .call(d3.axisLeft(y));
