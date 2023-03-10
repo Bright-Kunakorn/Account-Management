293bac3b538a912eeb650fdce2c63de45072d556
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-03-10 19:51:15 +0700

    oop

--- src/app/server/serverRead.go
+++ src/app/server/serverRead.go
@@ -13,56 +13,82 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+type Employee struct {
+	Client     *mongo.Client
+	Collection *mongo.Collection
+}
 
-	// Connect to MongoDB
+func NewEmployee() *Employee {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Get all documents from MongoDB
 	collection := client.Database("employee").Collection("employee")
-	filter := bson.M{} // empty filter to match all documents
-	cur, err := collection.Find(ctx, filter)
-	if err != nil {
-		log.Fatal(err)
+	return &Employee{
+		Client:     client,
+		Collection: collection,
 	}
-	defer cur.Close(ctx)
+}
+
+func (e *Employee) GetAllEmployees() ([]map[string]interface{}, error) {
+	filter := bson.M{} // empty filter to match all documents
+	cur, err := e.Collection.Find(context.Background(), filter)
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(context.Background())
 
 	// Create a slice of maps to hold the results
 	results := []map[string]interface{}{}
 
 	// Loop over the documents and add them to the results slice
-	for cur.Next(ctx) {
+	for cur.Next(context.Background()) {
 		var result bson.M
 		err := cur.Decode(&result)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 		results = append(results, result)
 	}
 
 	if err := cur.Err(); err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func (e *Employee) SaveAllEmployeesToJSONFile(fileName string) error {
+	// Get all employees from MongoDB
+	results, err := e.GetAllEmployees()
+	if err != nil {
+		return err
 	}
 
 	// Convert the results slice to JSON
 	jsonBytes, err := json.Marshal(results)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	jsonStr := string(jsonBytes)
 
 	// Write the JSON string to a file
-	err = ioutil.WriteFile("employee.json", []byte(jsonStr), 0644)
+	err = ioutil.WriteFile(fileName, []byte(jsonStr), 0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Updated documents")
+	return nil
+}
+
+func main() {
+	employee := NewEmployee()
+	err := employee.SaveAllEmployeesToJSONFile("employee.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Updated documents")
-
 }
