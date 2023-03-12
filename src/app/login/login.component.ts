f91d94b529d6c78f4ba8aa6e44b29a95754bc0fd
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-03-12 14:18:48 +0700

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,24 +5,31 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  public loginForm!: FormGroup
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
+  public loginForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
   public ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       username: [''],
-      password: ['']
-    })
+      password: [''],
+    });
   }
   public login() {
-    (this.loginForm.value.username, this.loginForm.value.password)
-    if (this.loginForm.value.username == "admin" && this.loginForm.value.password == "admin") {
+    this.loginForm.value.username, this.loginForm.value.password;
+    if (
+      this.loginForm.value.username == 'admin' &&
+      this.loginForm.value.password == 'admin'
+    ) {
       this.loginForm.reset();
-      this.router.navigate(["employee-dashboard"])
+      this.router.navigate(['employee-dashboard']);
     } else {
-      alert("Invalid username or password.");
+      alert('Invalid username or password.');
     }
-  };
+  }
 }
